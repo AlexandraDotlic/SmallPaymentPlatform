277b33c4ec67b3dbb638d2c978a3adb4b4e821c7
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-03-25 16:33:04 +0100

    WebClient Startup updated

--- WebClient/Startup.cs
+++ WebClient/Startup.cs
@@ -1,6 +1,10 @@
+using Core.Domain.Repositories;
+using Core.Infrastructure.DataAccess.EfCoreDataAccess;
+using EfCoreDataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +28,12 @@ namespace WebClient
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContextPool<EfCoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SmallPaymentPlatformDevConnection"));
+            });
+            services.AddScoped<ICoreUnitOfWork, EfCoreUnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
