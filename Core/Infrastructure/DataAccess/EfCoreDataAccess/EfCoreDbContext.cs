5bad0a87518d8307a5ebd15a0f6771a7c6ac0a1d
Aleksandra Dotlic <da-infg@infogram.com>
2021-03-25 15:57:52 +0100

    Core.Infrastructure.DataAccess - created EfCoreDataAccess class library and EfCoreDbContext class

---
+++ Core/Infrastructure/DataAccess/EfCoreDataAccess/EfCoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EfCoreDataAccess
+{
+    public class EfCoreDbContext : DbContext
+    {
+        public EfCoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
+
