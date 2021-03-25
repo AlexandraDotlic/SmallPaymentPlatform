8e0c9e3ebef6375451deb321a38efd1dda820327
Aleksandra Dotlic <da-infg@infogram.com>
2021-03-25 15:41:24 +0100

    Add project files.

---
+++ Core/Domain/Entities/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; private set; }
+        public decimal Amount { get; private set; }
+        public DateTime TransactionDateTime { get; private set; }
+        public TransactionType Type { get; private set; }
+
+
+    }
+}
---
+++ Core/Domain/Entities/Wallet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain.Entities
+{
+    public class Wallet
+    {
+        public string JMBG { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public BankType Bank { get; private set; }
+        public string BankAccountNumber { get; private set; }
+        public string BankPIN { get; private set; }
+        public decimal Balance { get; private set; }
+
+    }
+}
