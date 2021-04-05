7aedfc6e3d650c3413b114348d15dcfc9296593f
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-04-05 18:05:20 +0200

    Core.Domain.Services.Internal-  created FeeService and its implemetations

---
+++ Core/Domain/Services/Internal/FeeService/Implementations/FeeService.cs
@@ -0,0 +1,49 @@
+﻿using Core.Domain.Entities;
+using Core.Domain.Services.Internal.FeeService.Interface;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Services.Internal.FeeService.Implementations
+{
+    public class FeeService : IFeeService
+    {
+        private readonly int DaysAfterWalletCreationWithNoFee;
+        private readonly bool IsFirstTransferFreeInMonth;
+        private readonly decimal FixedFee;
+        private readonly int PercentageFee;
+        private readonly decimal FeeLimit;
+        public FeeService(IConfiguration configuration)
+        {
+            DaysAfterWalletCreationWithNoFee = Int32.Parse(configuration["DaysAfterWalletCreationWithNoFee"]);
+            IsFirstTransferFreeInMonth = bool.Parse(configuration["IsFirstTransferFreeInMonth"]);
+            FixedFee = decimal.Parse(configuration["FixedFee"]);
+            PercentageFee = Int32.Parse(configuration["PercentageFee"]);
+            FeeLimit = decimal.Parse(configuration["FeeLimit"]);
+        }
+        public async Task<decimal> CalculateTransferFee(Wallet wallet, decimal transferAmount)
+        {
+            if (wallet.WalletCreationTime.Date.AddDays(DaysAfterWalletCreationWithNoFee) > DateTime.Now.Date)
+            {
+                return 0m;
+            }
+
+            if (IsFirstTransferFreeInMonth && 
+                (wallet.LastTransferDateTime.Month != DateTime.Now.Month || wallet.LastTransferDateTime.Year != DateTime.Now.Year))
+            {
+                return 0m;
+            }
+
+            if (transferAmount < FeeLimit)
+            {
+                return FixedFee;
+            }
+            else
+            {
+                return (transferAmount * PercentageFee) / 100m;
+            }
+        }
+    }
+}
---
+++ Core/Domain/Services/Internal/FeeService/Interface/IFeeService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Services.Internal.FeeService.Interface
+{
+    public interface IFeeService
+    {
+        Task<decimal> CalculateTransferFee(Wallet wallet, decimal transferAmount);
+    }
+}
