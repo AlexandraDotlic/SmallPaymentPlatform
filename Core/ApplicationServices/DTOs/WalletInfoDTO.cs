7c48c65f2cf03c4412ee523f3a992b8a9e80c06c
Aleksandra Dotlic <alexandra.nerandzic@gmail.com>
2021-04-07 12:11:41 +0200

    Core.ApplicationServices.WalletService - implemented GetWalletInfo

---
+++ Core/ApplicationServices/DTOs/WalletInfoDTO.cs
@@ -0,0 +1,41 @@
+﻿using Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.ApplicationServices.DTOs
+{
+    public class WalletInfoDTO
+    {
+        public WalletInfoDTO(
+            string jMBG, 
+            string firstName, 
+            string lastName, 
+            BankType bank, 
+            string bankAccountNumber, 
+            decimal balance,
+            bool isBlocked, 
+            DateTime walletCreationTime)
+        {
+            JMBG = jMBG;
+            FirstName = firstName;
+            LastName = lastName;
+            Bank = bank;
+            BankAccountNumber = bankAccountNumber;
+            Balance = balance;
+            IsBlocked = isBlocked;
+            WalletCreationTime = walletCreationTime;
+        }
+
+        public string JMBG { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public BankType Bank { get; set; }
+        public string BankAccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsBlocked { get; set; }
+        public DateTime WalletCreationTime { get; set; }
+
+
+    }
+}
